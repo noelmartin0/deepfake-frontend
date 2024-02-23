81027133e179fbf00f57b73433f6e3e494bbe056
Noel Martin <noelmartin0@gmail.com>
2024-02-23 14:09:12 +0530

    final

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,28 +1,32 @@
 import Background from "../components/property1-variant";
 import Footer from "../components/footer";
-import Upload from "../components/upload";
 import Navbar from "../components/nav";
-import "./home.css";
+import "./result.css";
 
 const Home = () => {
   return (
     <div className="home">
       <header className="result-parent">
-      <Navbar/>
+        <Navbar/>
         <Background
           propTop="0px"
           propBackground="linear-gradient(127.22deg, #000 53.53%, #16431d)"
           propLeft="0px"
-          propWidth="100%"
-          propHeight="100%"
+          // propWidth="100%"
+          // propHeight="100%"
           propMargin="0 !important"
           propRight="0px"
           propBottom="0px"
         />
-        <div classname="result-area">
-          <h1 color="white">Jeloo</h1>
+        <div className="result-area2">
+          <img className="img-uploaded" src="/pexelsandreapiacquadio774909-1@2x.png"/>
+          <div className="deepfake-detector2">
+            <h1 className="deepfake-detectionio2">
+              <p className="deepfake2"><span className="deep">The video file upload is 97.2 %</span></p>
+              <p><span className="io2">Real.</span></p>
+            </h1>
+          </div>
         </div>
-        
         <Footer />
       </header>
     </div>
